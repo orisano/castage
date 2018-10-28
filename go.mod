3f7bbca5e0587a8be95cf58ddcc92e3674981db4
Nao YONASHIRO <owan.orisano@gmail.com>
2018-10-29 02:57:30 +0900

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/orisano/castage
+
+require (
+	github.com/docker/docker v0.0.0-20180531152204-71cd53e4a197
+	github.com/docker/go-connections v0.0.0-20180212134524-7beb39f0b969
+	github.com/docker/go-units v0.0.0-20170127094116-9e638d38cf69
+	github.com/moby/buildkit v0.3.1
+	github.com/pkg/errors v0.8.0
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/moby/buildkit/frontend/dockerfile/instructions"
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("castage: ")
+
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	dockerfilePath := flag.String("f", "Dockerfile", "Dockerfile path")
+	imageName := flag.String("i", "", "image name (required)")
+	buildContext := flag.String("p", ".", "build context")
+	push := flag.Bool("push", false, "push")
+	flag.Parse()
+
+	if *imageName == "" {
+		log.Print("-i <image name> is must be required")
+		invalidArgs()
+	}
+
+	stageNames, err := readStageNames(*dockerfilePath)
+	if err != nil {
+		return errors.Wrap(err, "failed to read stage names")
+	}
+
+	cachedStages := make([]string, 0, len(stageNames))
+	fmt.Println("set -ex")
+	for _, stageName := range stageNames {
+		cachedStage := fmt.Sprintf("%s:%s-cache", *imageName, stageName)
+		cachedStages = append(cachedStages, cachedStage)
+		fmt.Printf("docker pull %s || true\n", cachedStage)
+		fmt.Printf("docker build -t %s --target=%s --cache-from=%s %s\n", cachedStage, stageName, strings.Join(cachedStages, ","), *buildContext)
+		if *push {
+			fmt.Printf("docker push %s\n", cachedStage)
+		}
+	}
+
+	return nil
+}
+
+func readStageNames(dockerfilePath string) ([]string, error) {
+	var r io.Reader
+	if dockerfilePath == "-" {
+		r = os.Stdin
+	} else {
+		f, err := os.Open(dockerfilePath)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to open Dockerfile")
+		}
+		defer f.Close()
+		r = f
+	}
+	result, err := parser.Parse(r)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse Dockerfile")
+	}
+
+	stages, _, err := instructions.Parse(result.AST)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse instructions")
+	}
+
+	stageNames := make([]string, len(stages))
+	for i, stage := range stages {
+		if stage.Name == "" {
+			stage.Name = fmt.Sprint(i + 1)
+		}
+		stageNames[i] = stage.Name
+	}
+	return stageNames, nil
+}
+
+func invalidArgs() {
+	flag.Usage()
+	os.Exit(2)
+}
