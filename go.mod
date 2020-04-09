880d0e6d1e6bdbb4dd4acbe434a1233028670f2d
Nao YONASHIRO <owan.orisano@gmail.com>
2020-04-09 20:49:45 +0900

    feat: replace github.com/pkg/errors to golang.org/x/xerrors

--- go.mod
+++ go.mod
@@ -1,9 +1,15 @@
 module github.com/orisano/castage
 
+go 1.14
+
 require (
-	github.com/docker/docker v0.0.0-20180531152204-71cd53e4a197
-	github.com/docker/go-connections v0.0.0-20180212134524-7beb39f0b969
-	github.com/docker/go-units v0.0.0-20170127094116-9e638d38cf69
+	github.com/docker/docker v0.0.0-20180531152204-71cd53e4a197 // indirect
+	github.com/docker/go-connections v0.0.0-20180212134524-7beb39f0b969 // indirect
+	github.com/docker/go-units v0.0.0-20170127094116-9e638d38cf69 // indirect
+	github.com/google/go-cmp v0.4.0 // indirect
 	github.com/moby/buildkit v0.3.1
-	github.com/pkg/errors v0.8.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
+	gotest.tools v2.2.0+incompatible // indirect
 )
--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/moby/buildkit/frontend/dockerfile/instructions"
 	"github.com/moby/buildkit/frontend/dockerfile/parser"
-	"github.com/pkg/errors"
+	"golang.org/x/xerrors"
 )
 
 func main() {
@@ -36,7 +36,7 @@ func run() error {
 
 	stageNames, err := readStageNames(*dockerfilePath)
 	if err != nil {
-		return errors.Wrap(err, "failed to read stage names")
+		return xerrors.Errorf("read stage names: %w", err)
 	}
 
 	cachedStages := make([]string, 0, len(stageNames))
@@ -61,19 +61,19 @@ func readStageNames(dockerfilePath string) ([]string, error) {
 	} else {
 		f, err := os.Open(dockerfilePath)
 		if err != nil {
-			return nil, errors.Wrap(err, "failed to open Dockerfile")
+			return nil, xerrors.Errorf("open Dockerfile: %w", err)
 		}
 		defer f.Close()
 		r = f
 	}
 	result, err := parser.Parse(r)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to parse Dockerfile")
+		return nil, xerrors.Errorf("parse Dockerfile: %w", err)
 	}
 
 	stages, _, err := instructions.Parse(result.AST)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to parse instructions")
+		return nil, xerrors.Errorf("parse instructions: %w", err)
 	}
 
 	stageNames := make([]string, 0, len(stages))
